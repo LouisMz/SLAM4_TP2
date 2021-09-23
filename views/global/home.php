a20ac5df0bbd2c098c1effb39a5bb3e0e551e9b3
LouisLEBG <louis.maze49@gmail.com>
2021-09-23 11:33:29 +0200

    Ajout d'une photo acceuil mouton

--- views/global/home.php
+++ views/global/home.php
@@ -7,6 +7,7 @@
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, commodi delectus eaque eos esse ex illum laboriosam laborum modi, molestias necessitatibus non provident quidem quis repellat soluta tempore, veritatis vitae.
                 </p>
             </div>
+            <img src="./public/img/sheep.jpg" alt="moutons trop bg"/>
         </div>
     </div>
 </div>
