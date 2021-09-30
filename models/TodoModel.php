44b6573276a433fa84aef6c94b701e6667b97426
LouisLEBG <louis.maze49@gmail.com>
2021-09-30 11:18:37 +0200

    tq cestr bien

--- models/TodoModel.php
+++ models/TodoModel.php
@@ -10,8 +10,16 @@ class TodoModel extends SQL
         parent::__construct('todos', 'id');
     }
 
+    function todoNonTermine()
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM todos WHERE termine = 1;");
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
     function marquerCommeTermine($id){
         $stmt = $this->pdo->prepare("UPDATE todos SET termine =1 WHERE id = ?");
         $stmt->execute([$id]);
     }
+    
 }
--- routes/Web.php
+++ routes/Web.php
@@ -5,6 +5,7 @@ namespace routes;
 use controllers\Account;
 use controllers\SampleWeb;
 use controllers\VideoWeb;
+use controllers\TodoWeb;
 use routes\base\Route;
 use utils\SessionHelpers;
 
@@ -16,6 +17,13 @@ class Web
 
         Route::Add('/', [$main, 'home']);
         Route::Add('/about', [$main, 'about']);
+
+        $todo = new TodoWeb();
+
+        Route::Add('/todo/liste', [$todo, 'liste']);
+        Route::Add('/todo/ajouter', [$todo, 'ajouter']);
+        Route::Add('/todo/terminer', [$todo, 'terminer']);
+        Route::Add('/todo/supprimer', [$todo, 'supprimer']);
 
         //        Exemple de limitation d'accès à une page en fonction de la SESSION.
         //        if (SessionHelpers::isLogin()) {
