77f8a5141c0461bab35ac4aafaf717c8fe9f78e9
LouisLEBG <louis.maze49@gmail.com>
2021-09-30 15:53:09 +0200

    trse

--- views/todo/liste.php
+++ views/todo/liste.php
@@ -17,9 +17,22 @@
                         <div class="d-flex">
                             <div class="flex-grow-1 align-self-center"><?= $todo['texte'] ?></div>
                             <div>
+                                <?php 
+                                if($todo['termine']==0){
+                                ?>
                                 <a href="./terminer?id=<?= $todo['id'] ?>" class="btn btn-outline-success">
-                                    <i class="bi bi-check"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-check-fill" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+                                    </svg>
                                 </a>
+                                <?php } 
+                                else{ ?>
+                                <a href="./supprimer?id<?= $todo['id'] ?>" class="btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-check-fill" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+                                    </svg>
+                                </a>
+                                <?php } ?>
                                 <!-- Action à ajouter pour Supprimer -->
                             </div>
                         </div>
